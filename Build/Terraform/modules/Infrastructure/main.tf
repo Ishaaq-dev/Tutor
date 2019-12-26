7a2d8d677ed0f6c0fd1d94b76f92ab1fc42f3fff
Is'haaq Iqbal <Ishaaq@Zafars-Mac-mini.local>
2019-12-26 14:48:08 +0000

    adds terraform files and dirs

---
+++ Build/Terraform/modules/Infrastructure/main.tf
@@ -0,0 +1,22 @@
+variable "prefix" {
+  default = "dev"
+}
+
+resource "aws_dynamodb_table" "available-times-dynamodb-table" {
+  name           = "${var.prefix}-available-times-dynamo-db"
+  billing_mode   = "PAY-PER-REQUEST"
+  read_capacity  = 3
+  write_capacity = 3
+  hash_key       = "AvailableTimes"
+  range_key      = "Day"
+
+  attribute {
+    name = "AvailableTimes"
+    type = "N"
+  }
+
+  attribute {
+    name = "Day"
+    type = "S"
+  }
+}
